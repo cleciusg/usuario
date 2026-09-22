11b09063c665a353e909538215758be8c5628740
Clecius <cleciusgustavo0702@gmail.com>
2026-09-22 20:11:07 -0300

    Adicionado classes de security

---
+++ src/main/java/com/gustavo/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.gustavo.usuario.infrastructure.repository;
+
+import com.javaaprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
+}
